d7dce3140c8c66bcabb8ba6cb6553a0a4a61e779
Боткин Артём <wam2d55@gmail.com>
2025-10-09 18:52:19 +0300

    Добавлены финальные файлы лабораторной работы №6: отчет и тестовые запросы

--- test_queries.sql
+++ test_queries.sql
@@ -1,24 +1,4 @@
-
-SELECT COUNT(*) AS TotalStudents FROM Students;
-
-
-SELECT DISTINCT Subject FROM Teachers;
-
-
-SELECT MIN(Grade) AS MinGrade, MAX(Grade) AS MaxGrade FROM Grades;
-
-
-SELECT S.Name, T.FullName, G.Subject, G.Grade
-FROM Students AS S
-INNER JOIN Grades AS G ON S.ID = G.StudentID
-INNER JOIN Teachers AS T ON G.Subject = T.Subject;
-
-
-
-SELECT Name, COUNT(*) as DuplicateCount 
-FROM Students 
-GROUP BY Name 
-HAVING COUNT(*) > 1;
-
-
-SELECT * FROM Grades WHERE Grade < 2 OR Grade > 5;
+SELECT MIN(Grade) AS MinGrade, MAX(Grade) AS MaxGrade FROM Grades;  -- Тестовые запросы для лабораторной работы №6 
+SELECT COUNT(*) AS TotalStudents FROM Students; 
+SELECT DISTINCT Subject FROM Teachers; 
+SELECT MIN(Grade) AS MinGrade, MAX(Grade) AS MaxGrade FROM Grades; 
